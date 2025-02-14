5a4960d9e7ada927eed6ff6e10e0a56640317cbb
Dheeraj-2003 <dheerajsolanki203@gmail.com>
2025-02-14 23:01:19 +0530

    dto refractor

--- src/todo/dto/createTodoDto.ts
+++ src/todo/dto/createTodoDto.ts
@@ -1,9 +1,15 @@
 import { IsNotEmpty, Length } from "class-validator";
+import { Todo } from "../todo.interface";
 
-export class CreateTodoDto{
+export class CreateTodoDto implements Todo{
+    id: number;
+
     @IsNotEmpty()
     @Length(1,255)
     title: string;
 
     completed:boolean
+
+    description?: string;
+    created_at: Date;
 }
